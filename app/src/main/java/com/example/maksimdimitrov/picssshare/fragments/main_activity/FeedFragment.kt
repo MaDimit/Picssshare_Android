f24ef2a0f1ef2308b3754d5662ab5ec4824bec86
Maxim Dimitrov <mdimirtov@gmail.com>
2018-06-22 11:18:19 +0300

    Feed fragment init

---
+++ app/src/main/java/com/example/maksimdimitrov/picssshare/fragments/main_activity/FeedFragment.kt
@@ -0,0 +1,36 @@
+package com.example.maksimdimitrov.picssshare.fragments.main_activity
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.maksimdimitrov.picssshare.R
+
+
+class FeedFragment : Fragment() {
+    private lateinit var listener: FeedInteractionListener
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is FeedInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement FeedInteractionListener")
+        }
+    }
+
+    interface FeedInteractionListener {
+        fun onFeedItemClick(item : Int)
+    }
+
+}
