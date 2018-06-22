a49d8d3be81823da5406027e0c164754c011d371
Maxim Dimitrov <mdimirtov@gmail.com>
2018-06-22 11:20:23 +0300

    Activity main init

--- app/src/main/java/com/example/maksimdimitrov/picssshare/controller/MainActivity.kt
+++ app/src/main/java/com/example/maksimdimitrov/picssshare/controller/MainActivity.kt
@@ -1,21 +1,68 @@
 package com.example.maksimdimitrov.picssshare.controller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v7.app.AppCompatActivity
 import com.example.maksimdimitrov.picssshare.R
+import com.example.maksimdimitrov.picssshare.fragments.main_activity.*
 import com.example.maksimdimitrov.picssshare.model.User
 import com.example.maksimdimitrov.picssshare.utilities.EXTRA_USER
+import com.example.maksimdimitrov.picssshare.utilities.toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
-    lateinit var user : User
+const val FRAGMENT_FEED = "feed_fragment"
+
+class MainActivity : AppCompatActivity()
+        , BottomNavigationFragment.BottomNavigationInteractionListener
+        , FeedFragment.FeedInteractionListener
+        , SearchFragment.SearchInteractionListener
+        , UploadFragment.UploadInteractionListener
+        , AccountFragment.AccountInteractionListener
+        , FavoritesFragment.FavoritesInteractionListener{
+
+    override fun onBottomNavigationItemClick(item: Int) {
+        when(item) {
+            NAVIGATION_HOME -> toast("Home clicked")
+            NAVIGATION_SEARCH -> toast("Search clicked")
+            NAVIGATION_UPLOAD -> toast("Upload clicked")
+            NAVIGATION_FAVORITES -> toast("Favorites clicked")
+            NAVIGATION_ACCOUNT -> toast("Account clicked")
+        }
+    }
+
+    override fun onFeedItemClick(item: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSearchItemClick(item: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUploadItemClick(item: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAccountItemClick(item: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onFavoritesItemClick(item: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    lateinit var user: User
+    private lateinit var fm: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         user = intent.getParcelableExtra(EXTRA_USER)
-        hi.text = "Hi, $user"
+        fm = supportFragmentManager
+        savedInstanceState?.let {
+            fm.beginTransaction()
+                    .replace(fragment_container.id, FeedFragment(), FRAGMENT_FEED)
+                    .commit()
+        }
     }
 }
